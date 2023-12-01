6fbcf6a123b66abb11326f40dc60d74f350147c5
Krishna Koganti <KrishnavKoganti@users.noreply.github.com>
2023-12-01 11:43:22 -0800

    clean up

--- weekend-wiz/src/App.tsx
+++ weekend-wiz/src/App.tsx
@@ -1,21 +1,6 @@
 import EventCalendar from "./components/EventCalendar"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login.js";
-// function App() {
-
-//   <div className="app">
-//   <Router>
-//     <Routes>
-//     <Route path="/" element={<Login />} />
-//     </Routes>
-//   </Router>
-// </div>
-//   // return (
-//   //   <>
-//   //     <EventCalendar />
-//   //   </>
-//   // )
-// }
 
 import Register from "./components/Register";
 import Reset from "./components/Reset";
